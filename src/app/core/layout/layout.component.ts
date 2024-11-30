abb63db8141e9c59d1c65dd003be8309140ea452
khizaruddins <khizaruddins@gmail.com>
2024-11-30 15:03:26 +0530

    cookie service and storage service added

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,16 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { VarsService } from '../../shared/services/vars.service';
+import { IMainLayout } from '../../shared/models/layout.interface';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
+  vars = inject(VarsService);
+  layoutConfig: IMainLayout = {showHeader: true, showFooter: true};
 
+  ngOnInit() {
+    this.vars.mainLayoutConfig$.subscribe({next: val =>  {
+      this.layoutConfig = val;
+      console.log(this.layoutConfig);
+    }})
+  }
 }
